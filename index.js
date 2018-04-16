ae1ee5e98fcc398bef6f60305d8c6681556f1434
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-04-16 15:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.")) {
+    return "I love you, too."
+  }
 }
