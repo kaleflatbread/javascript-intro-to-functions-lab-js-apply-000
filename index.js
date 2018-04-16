08b2ce97324123ec49113e769b9ac6dfedb2bb89
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-04-16 15:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ function whisper(string) {
 }
 
 function logShout(string){
-  console.log() string.toUpperCase
+  console.log() string.toUpperCase()
 }
 
-function
+function logWhisper(string){
+  console.log() string.toLowerCase()
+}
+
+function sayHiToGrandma(string){
+  return
+}
